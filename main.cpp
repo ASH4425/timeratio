ca5ee38d21a86aad18fd26dcce10b213effe626b
Sohyun An <thgus4425@naver.com>
2020-08-05 14:41:45 +0900

    Use r 0

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("D2D=3.5.csv");                                                                                                            
+	mywriteoutfile.open("0-0.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
         //cout << "Training Epoch : " << i << endl;

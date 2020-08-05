900760fb763772107fcb6c11b5f73c0288bd32ab
Sohyun An <thgus4425@naver.com>
2020-08-05 11:38:25 +0900

    C2C=3.5

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("4.csv");                                                                                                            
+	mywriteoutfile.open("C2C=3.5.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
         //cout << "Training Epoch : " << i << endl;

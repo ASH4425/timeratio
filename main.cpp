724a38b4b4ac5b1cb80f329265363af150396352
Sohyun An <thgus4425@naver.com>
2020-08-18 21:28:01 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("withoutDrift.csv");                                                                                                            
+	mywriteoutfile.open("withoutDriftRe.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
         //cout << "Training Epoch : " << i << endl;

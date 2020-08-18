cb961e51849766b34a66777f76079932ef6c3c0b
Sohyun An <thgus4425@naver.com>
2020-08-18 10:23:59 +0900

    Use batch 4000

--- Param.cpp
+++ Param.cpp
@@ -46,7 +46,7 @@ Param::Param() {
 	numMnistTestImages = 10000;	// # of testing images in MNIST
 	
 	/* Algorithm parameters */
-	numTrainImagesPerEpoch = 8000;	// # of training images per epoch
+	numTrainImagesPerEpoch = 4000;	// # of training images per epoch
 	totalNumEpochs = 125;	// Total number of epochs
 	interNumEpochs = 1;		// Internal number of epochs (print out the results every interNumEpochs)
 	nInput = 400;     // # of neurons in input layer
